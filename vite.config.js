f7e1dd563c2284c4025d2e862720e2be0628f7ff
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2023-10-31 07:29:10 +0700

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./disc', import.meta.url))
+      '@': fileURLToPath(new URL('./dist', import.meta.url))
     }
   }
 })
